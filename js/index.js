bd04d14bab388a330fda7a4f4feae16821c4eb82
Adam Honoré <adam.honore@gmail.com>
2015-04-07 14:06:06 +0200

    updated index, added input fields

--- js/index.js
+++ js/index.js
@@ -3,7 +3,13 @@ var data = {};
 
 function onContinue() {
 	data.person = {
-		name: $('input[name="name"]').val()
+		reference: $('input[name="reference"]').val(),
+		age: $('input[name="age"]').val(),
+		gender: $('input[name="gender"]:checked').val(),
+		videogames: $('select[name="videogames"]').val(),
+		computers: $('select[name="computers"]').val(),
+		hand: $('input[name="hand"]:checked').val(),
+		device: $('select[name="device"]').val()
 	};
 	sessionStorage.data = JSON.stringify(data);
 	window.location.replace('pointing.html');
