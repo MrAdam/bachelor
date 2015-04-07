1aac9a8ae07ced5a46551d19952a7d33e466d6c7
Bajinai <bajinalphi@gmail.com>
2015-04-07 14:08:05 +0200

    Navigating task

--- js/navigating.js
+++ js/navigating.js
@@ -3,24 +3,58 @@ paper.install(window);
 var pathTool, path;
 var center, target;
 var running = false;
-var startW = [20, 30, 40, 50];
+var W = [20, 30, 40, 50];
 var endW = 8;
-var A = [250, 500, 750, 1000];
+var A = [100, 250, 300, 400];
+var topline, finishline, botline, startline;
+var x1, x2, y1, y2, avalue;
+
+function next() {
+	if(A.length > 0)
+	topline.remove();
+	botline.remove();
+	finishline.remove();
+	startline.remove();
+	path.remove();
+	avalue = A.shift();
+	wvalue = W.shift();
+	x1 = view.center.x-(avalue/2);
+	y1 = view.center.y-(wvalue/2);
+	x2 = view.center.x+(avalue/2);
+	y2 = view.center.y+(wvalue/2);
+	topline = new Path.Line(new Point(x1,y1), new Point(x2,y1));
+	botline = new Path.Line(new Point(x1,y2), new Point(x2,y1+8));
+	startline = new Path.Rectangle(new Point(x1,y1), new Point(x1-3,y2));
+	finishline = new Path.Rectangle(new Point(x2,y1), new Point(x2+3,y1+8));
+	topline.strokeColor = "black";
+	botline.strokeColor = "black";
+	startline.strokeColor = "green";
+	startline.fillColor = "green";
+	finishline.strokeColor = "green";
+	finishline.fillColor = "green";
+}
 
 window.onload = function() {
 	
 	paper.setup('canvas');
-
-	var topline = new Path.Line(new Point(30,250), new Point(500,250));
-	topline.strokeColor = "black"
-	botline = new Path.Line(new Point(30,270), new Point(500,258));
+	avalue = A.shift();
+	wvalue = W.shift();
+	x1 = view.center.x-(avalue/2);
+	y1 = view.center.y-(wvalue/2);
+	x2 = view.center.x+(avalue/2);
+	y2 = view.center.y+(wvalue/2);
+	
+	topline = new Path.Line(new Point(x1,y1), new Point(x2,y1));
+	topline.strokeColor = "black";
+	
+	botline = new Path.Line(new Point(x1,y2), new Point(x2,y1+8));
 	botline.strokeColor = "black";
 	
-	var startline = new Path.Rectangle(new Point(30,250), new Point(32,270));
+	startline = new Path.Rectangle(new Point(x1,y1), new Point(x1-3,y2));
 	startline.strokeColor = "green";
 	startline.fillColor = "green";
 	
-	var finishline = new Path.Rectangle(new Point(500,250), new Point(502,258));
+	finishline = new Path.Rectangle(new Point(x2,y1), new Point(x2+3,y1+8));
 	finishline.strokeColor = "green";
 	finishline.fillColor = "green";
 	
@@ -34,10 +68,11 @@ window.onload = function() {
 		}
 		if (startline.hitTest(event.point) != null && running == false && event.delta.x > 0) {
 			path = new Path();
+			running = true;
 			path.strokeColor = 'blue';
 			startline.strokeColor = "red";
 			startline.fillColor = "red";
-			running = true;
+			return;
 		}
 		
 		if (finishline.hitTest(event.point) != null && running == true && event.delta.x > 0) {
@@ -47,10 +82,8 @@ window.onload = function() {
 			startline.fillColor = "yellow";
 			finishline.fillColor = "yellow";
 			running = false;
-			
-			
-			
-			
+			nextButton = $("#nextTunnel");
+			nextButton.show();
 		}
 		
 	}
