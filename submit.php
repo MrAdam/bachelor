69d66fd9f6bdfb3e22aeba666d3cbdeb77e1e6f0
Adam Honoré <adam.honore@gmail.com>
2015-04-15 11:22:45 +0200

    update

--- submit.php
+++ submit.php
@@ -49,6 +49,31 @@
 		}
 	}
 	
+	foreach ($_POST['pointing'] as $task) {
+		$person 	= $insert_person;
+		$type 		= '"'.$mysqli->real_escape_string('pointing').'"';
+		$distance 	= (int)$task['distance'];
+		$width		= (int)$task['width'];
+		$time		= (float)$task['time'];
+		$insert_task = $mysqli->query("INSERT INTO task (person, type, distance, width, time) VALUES ($person, $type, $distance, $width, $time)");
+		if (!$insert_task)
+			die('Error : ('. $mysqli->errno .') '. $mysqli->error);
+		else
+			$insert_task = $mysqli->insert_id;
+		
+		foreach ($task['points'] as $point) {
+			$task			= $insert_task;
+			$x				= (int)$point['x'];
+			$y				= (int)$point['y'];
+			$elapsedTime	= (float)$point['elapsedTime'];
+			$deltaTime		= (float)$point['deltaTime'];
+			$deltaDistance 	= (float)$point['deltaDistance'];
+			$insert_point = $mysqli->query("INSERT INTO point (task, x, y, elapsedTime, deltaTime, deltaDistance) VALUES ($task, $x, $y, $elapsedTime, $deltaTime, $deltaDistance)");
+			if (!$insert_point)
+				die('Error : ('. $mysqli->errno .') '. $mysqli->error);
+		}
+	}
+	
 	$mysqli->close();
 	
 	die('Tak for din deltagelse. Dine data er blevet gemt korrekt.');
