75fb3cf21b3d5b76843de510df7324d3c3345e46
Adam Honoré <adam.honore@gmail.com>
2015-04-15 10:32:23 +0200

    update

--- submit.php
+++ submit.php
@@ -10,7 +10,20 @@
 	if ($mysqli->connect_error)
 		die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
 	
+	$reference = $_POST['person']['reference'];
+	$age = $_POST['person']['age'];
+	$gender = $_POST['person']['gender'];
+	$videogames = $_POST['person']['videogames'];
+	$computers = $_POST['person']['computers'];
+	$hand = $_POST['person']['hand'];
+	$device = $_POST['person']['device'];
+	
+	for ($i = 0; $i < 10; $i++) {
+		
+	}
+	
 	$mysqli->close();
+	
 	
 	var_dump($_POST);
 	
