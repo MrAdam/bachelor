92598a815a613a2b7990cf4897e05ea913378d8f
Adam Honoré <adam.honore@gmail.com>
2015-04-15 10:27:24 +0200

    update

--- submit.php
+++ submit.php
@@ -1,5 +1,15 @@
 <?php
 	
-	var_dump($_POST);
+	if (!isset($_POST['person']))
+		die('Validation error!');
+	
+	if (!isset($_POST['navigating']))
+		die('Validation error!');
+	
+	$mysqli = new mysqli('127.0.0.1','root','toor','bachelor');
+	if ($mysqli->connect_error)
+		die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+	
+	$mysqli->close();
 	
 ?>
