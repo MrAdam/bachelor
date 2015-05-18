2620622bfa8e290defe0f40afdb3d4b1b0887884
Adam Honoré <adam.honore@gmail.com>
2015-05-18 10:14:31 +0200

    updated analysis with new plots

--- latex/analyse.r
+++ latex/analyse.r
@@ -2,6 +2,7 @@
 library(DBI);
 library(RMySQL);
 library(plotrix);
+library(ggplot2)
 
 # Disable warnings temporarily
 options(warn=-1);
@@ -37,29 +38,25 @@ data <- tasks[tasks$type == "pointing", c("distance", "width", "time")];
 fitt_data <- data;
 fitt_data$id <- log2((2 * fitt_data$distance) / (fitt_data$width));
 fitt_model <- lm(time ~ id, data = fitt_data);
-layout(matrix(c(1,2,3,4),2,2));
-plot(fitt_model);
+print(ggplot(data = fitt_data, aes(x = id, y = time)) + geom_point() + geom_smooth(method="lm", formula = y ~ x));
 
 # Welford's model
 welford_data <- data;
 welford_data$id <- log2((welford_data$distance + 0.5 * welford_data$width) / (welford_data$width));
 welford_model <- lm(time ~ 0 + id, data = welford_data);
-layout(matrix(c(1,2,3,4),2,2));
-plot(welford_model);
+print(ggplot(data = welford_data, aes(x = id, y = time)) + geom_point() + geom_smooth(method="lm", formula = y ~ 0 + x));
 
 # MacKenzie's model
 mackenzie_data <- data;
 mackenzie_data$id <- log2((mackenzie_data$distance + mackenzie_data$width) / (mackenzie_data$width));
 mackenzie_model <- lm(time ~ id, data = mackenzie_data);
-layout(matrix(c(1,2,3,4),2,2));
-plot(mackenzie_model);
+print(ggplot(data = mackenzie_data, aes(x = id, y = time)) + geom_point() + geom_smooth(method="lm", formula = y ~ x));
 
 # Meyer's model
 meyer_data <- data;
 meyer_data$id <- sqrt((meyer_data$distance) / (meyer_data$width));
 meyer_model <- lm(time ~ id, data = meyer_data);
-layout(matrix(c(1,2,3,4),2,2));
-plot(meyer_model);
+print(ggplot(data = meyer_data, aes(x = id, y = time)) + geom_point() + geom_smooth(method="lm", formula = y ~ x));
 
 # AIC analysis
 print(AIC(fitt_model, welford_model, mackenzie_model, meyer_model));
