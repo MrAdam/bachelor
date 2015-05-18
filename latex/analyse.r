6569bd4c954f5b0f82bbe5d4c337c1a27a3527a0
Adam Honoré <adam.honore@gmail.com>
2015-05-18 19:46:34 +0200

    updated r-code with new plots

--- latex/analyse.r
+++ latex/analyse.r
@@ -4,16 +4,6 @@ library(RMySQL);
 library(plotrix);
 library(ggplot2)
 
-# User-defined functions
-qqplot <- function(linearModel, title) {
-  y <- quantile(linearModel$resid[!is.na(linearModel$resid)], c(0.25, 0.75));
-  x <- qnorm(c(0.25, 0.75));
-  slope <- diff(y) / diff(x);
-  int <- y[1L] - slope * x[1L];
-  plot <- ggplot(linearModel, aes(sample=.resid)) + stat_qq(alpha=0.5) + geom_abline(slope=slope, intercept=int, color="blue") + labs(title=title);
-  return(plot);
-}
-
 # Disable warnings temporarily
 options(warn=-1);
 
@@ -48,29 +38,29 @@ data <- tasks[tasks$type == "pointing", c("distance", "width", "time")];
 fitt_data <- data;
 fitt_data$id <- log2((2 * fitt_data$distance) / (fitt_data$width));
 fitt_model <- lm(time ~ id, data = fitt_data);
-ggplot(data = fitt_data, aes(x = id, y = time)) + geom_point() + geom_smooth(method="lm", formula = y ~ x) + labs(title="Fitt's model");
-qqplot(fitt_model, "Fitt's QQ-plot");
+ggplot(data = fitt_model, aes(x = .fitted, y = .stdresid)) + geom_point() + 
+  labs(title="Fitt's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # Welford's model
 welford_data <- data;
 welford_data$id <- log2((welford_data$distance + 0.5 * welford_data$width) / (welford_data$width));
 welford_model <- lm(time ~ 0 + id, data = welford_data);
-ggplot(data = welford_data, aes(x = id, y = time)) + geom_point() + geom_smooth(method="lm", formula = y ~ 0 + x) + labs(title="Welford's model");
-qqplot(welford_model, "Welford's QQ-plot");
+ggplot(data = welford_model, aes(x = .fitted, y = .stdresid)) + geom_point() + 
+  labs(title="Welford's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # MacKenzie's model
 mackenzie_data <- data;
 mackenzie_data$id <- log2((mackenzie_data$distance + mackenzie_data$width) / (mackenzie_data$width));
 mackenzie_model <- lm(time ~ id, data = mackenzie_data);
-ggplot(data = mackenzie_data, aes(x = id, y = time)) + geom_point() + geom_smooth(method="lm", formula = y ~ x) + labs(title="MacKenzie's model");
-qqplot(mackenzie_model, "MacKenzie's QQ-plot");
+ggplot(data = mackenzie_model, aes(x = .fitted, y = .stdresid)) + geom_point() + 
+  labs(title="MacKenzie's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # Meyer's model
 meyer_data <- data;
 meyer_data$id <- sqrt((meyer_data$distance) / (meyer_data$width));
 meyer_model <- lm(time ~ id, data = meyer_data);
-ggplot(data = meyer_data, aes(x = id, y = time)) + geom_point() + geom_smooth(method="lm", formula = y ~ x) + labs(title="Meyer's model");
-qqplot(meyer_model, "Meyer's QQ-plot");
+ggplot(data = meyer_model, aes(x = .fitted, y = .stdresid)) + geom_point() + 
+  labs(title="Meyer's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # AIC analysis
 x <- c("Fitt's", "Welford's", "MacKenzie's", "Meyer's");
