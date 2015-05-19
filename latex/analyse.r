e9856275cad9f7d4c54303dae0c7fc3f554777af
Adam Honoré <adam.honore@gmail.com>
2015-05-19 13:42:44 +0200

    updated plots again, and sorted the AIC plot

--- latex/analyse.r
+++ latex/analyse.r
@@ -1,8 +1,8 @@
 # Load required libraries
 library(DBI);
 library(RMySQL);
-library(plotrix);
 library(ggplot2);
+library(gridExtra)
 
 # Disable warnings temporarily
 options(warn=-1);
@@ -16,7 +16,7 @@ persons <- dbFetch(result, n=-1);
 dbClearResult(result);
 
 # Fetch all data from the 'task' table
-result <- dbSendQuery(connection, "SELECT * FROM task WHERE id NOT IN (8094, 1741, 7847)");
+result <- dbSendQuery(connection, "SELECT * FROM task");
 tasks <- dbFetch(result, n=-1);
 dbClearResult(result);
 
@@ -32,37 +32,40 @@ dbDisconnect(connection);
 options(warn=0);
 
 # Filter out all data, except pointing data
-data <- tasks[tasks$type == "pointing", c("distance", "width", "time")];
+data <- tasks[tasks$type == "pointing" & tasks$time < 3000 & tasks$time != 0, c("distance", "width", "time")];
 
 # Fitt's model
 fitt_data <- data;
 fitt_data$id <- log2((2 * fitt_data$distance) / (fitt_data$width));
 fitt_model <- lm(time ~ id, data = fitt_data);
-ggplot(data = fitt_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
+fitt_plot <- ggplot(data = fitt_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
   labs(title="Fitt's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # Welford's model
 welford_data <- data;
 welford_data$id <- log2((welford_data$distance + 0.5 * welford_data$width) / (welford_data$width));
 welford_model <- lm(time ~ 0 + id, data = welford_data);
-ggplot(data = welford_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
+welford_plot <- ggplot(data = welford_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
   labs(title="Welford's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # MacKenzie's model
 mackenzie_data <- data;
 mackenzie_data$id <- log2((mackenzie_data$distance + mackenzie_data$width) / (mackenzie_data$width));
 mackenzie_model <- lm(time ~ id, data = mackenzie_data);
-ggplot(data = mackenzie_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
+mackenzie_plot <- ggplot(data = mackenzie_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
   labs(title="MacKenzie's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # Meyer's model
 meyer_data <- data;
 meyer_data$id <- sqrt((meyer_data$distance) / (meyer_data$width));
 meyer_model <- lm(time ~ id, data = meyer_data);
-ggplot(data = meyer_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
+meyer_plot <- ggplot(data = meyer_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
   labs(title="Meyer's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
+# Plots
+grid.arrange(fitt_plot, mackenzie_plot, welford_plot, meyer_plot, ncol=2);
+
 # AIC analysis
-x <- c("Fitt's", "Welford's", "MacKenzie's", "Meyer's");
-y <- c(AIC(fitt_model), AIC(welford_model), AIC(mackenzie_model), AIC(meyer_model));
-ggplot(data = NULL, aes(x=x, y=y, color=x)) + geom_point(aes(size=10)) + labs(title="AIC Analyse", x="Model", y="AIC") + theme(legend.position = "none");
+comparison <- data.frame(Model = c("Fitt's", "Welford's", "MacKenzie's", "Meyer's"), AIC = c(AIC(fitt_model), AIC(welford_model), AIC(mackenzie_model), AIC(meyer_model)));
+comparison$order <- reorder(comparison$Model, comparison$AIC);
+ggplot(comparison, aes(x = reorder(Model, AIC), y = AIC, color = Model)) + geom_point(aes(size=10)) + labs(title="AIC Analyse", x="Model") + theme(legend.position = "none");
