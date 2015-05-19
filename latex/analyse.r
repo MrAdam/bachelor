ca683edfea1beb123be56b7ff5150e9097fd891a
Adam Honoré <adam.honore@gmail.com>
2015-05-19 14:23:01 +0200

    added GAM line to plot - shows deviation from zero

--- latex/analyse.r
+++ latex/analyse.r
@@ -2,6 +2,7 @@
 library(DBI);
 library(RMySQL);
 library(ggplot2);
+library(grid);
 library(gridExtra)
 
 # Disable warnings temporarily
@@ -38,28 +39,28 @@ data <- tasks[tasks$type == "pointing" & tasks$time < 3000 & tasks$time != 0, c(
 fitt_data <- data;
 fitt_data$id <- log2((2 * fitt_data$distance) / (fitt_data$width));
 fitt_model <- lm(time ~ id, data = fitt_data);
-fitt_plot <- ggplot(data = fitt_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
+fitt_plot <- ggplot(data = fitt_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() + geom_smooth(se = F, color="red") +
   labs(title="Fitt's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # Welford's model
 welford_data <- data;
 welford_data$id <- log2((welford_data$distance + 0.5 * welford_data$width) / (welford_data$width));
 welford_model <- lm(time ~ 0 + id, data = welford_data);
-welford_plot <- ggplot(data = welford_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
+welford_plot <- ggplot(data = welford_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() + geom_smooth(se = F, color="red") +
   labs(title="Welford's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # MacKenzie's model
 mackenzie_data <- data;
 mackenzie_data$id <- log2((mackenzie_data$distance + mackenzie_data$width) / (mackenzie_data$width));
 mackenzie_model <- lm(time ~ id, data = mackenzie_data);
-mackenzie_plot <- ggplot(data = mackenzie_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
+mackenzie_plot <- ggplot(data = mackenzie_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() + geom_smooth(se = F, color="red") +
   labs(title="MacKenzie's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # Meyer's model
 meyer_data <- data;
 meyer_data$id <- sqrt((meyer_data$distance) / (meyer_data$width));
 meyer_model <- lm(time ~ id, data = meyer_data);
-meyer_plot <- ggplot(data = meyer_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
+meyer_plot <- ggplot(data = meyer_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() + geom_smooth(se = F, color="red") +
   labs(title="Meyer's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # Plots
