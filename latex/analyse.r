cf430c62632f60109f9e3148ee7e0508f1c5738c
Adam Honoré <adam.honore@gmail.com>
2015-05-19 10:00:14 +0200

    updated analysis with new plots

--- latex/analyse.r
+++ latex/analyse.r
@@ -38,28 +38,28 @@ data <- tasks[tasks$type == "pointing", c("distance", "width", "time")];
 fitt_data <- data;
 fitt_data$id <- log2((2 * fitt_data$distance) / (fitt_data$width));
 fitt_model <- lm(time ~ id, data = fitt_data);
-ggplot(data = fitt_model, aes(x = .fitted, y = .stdresid)) + geom_point() + 
+ggplot(data = fitt_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
   labs(title="Fitt's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # Welford's model
 welford_data <- data;
 welford_data$id <- log2((welford_data$distance + 0.5 * welford_data$width) / (welford_data$width));
 welford_model <- lm(time ~ 0 + id, data = welford_data);
-ggplot(data = welford_model, aes(x = .fitted, y = .stdresid)) + geom_point() + 
+ggplot(data = welford_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
   labs(title="Welford's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # MacKenzie's model
 mackenzie_data <- data;
 mackenzie_data$id <- log2((mackenzie_data$distance + mackenzie_data$width) / (mackenzie_data$width));
 mackenzie_model <- lm(time ~ id, data = mackenzie_data);
-ggplot(data = mackenzie_model, aes(x = .fitted, y = .stdresid)) + geom_point() + 
+ggplot(data = mackenzie_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
   labs(title="MacKenzie's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # Meyer's model
 meyer_data <- data;
 meyer_data$id <- sqrt((meyer_data$distance) / (meyer_data$width));
 meyer_model <- lm(time ~ id, data = meyer_data);
-ggplot(data = meyer_model, aes(x = .fitted, y = .stdresid)) + geom_point() + 
+ggplot(data = meyer_model, aes(x = .fitted, y = .stdresid)) + geom_hline(yintercept=0, alpha=0.75, color="blue") + geom_point() +
   labs(title="Meyer's Residualplot", x="Fitted", y="Standardiserede Residualer");
 
 # AIC analysis
