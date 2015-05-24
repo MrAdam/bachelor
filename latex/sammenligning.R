10051d4fab6c4d26b3d8c193480ce744fc814edd
Rasmus W. Andersen <williras@gmail.com>
2015-05-24 14:17:53 +0200

    graf

--- latex/sammenligning.R
+++ latex/sammenligning.R
@@ -1,10 +1,10 @@
-curve( sqrt(x), from=0, to=5, col="red", xlab="x", ylab="y")
+curve( sqrt(x), from=0, to=5, col="red", xlab="A/W", ylab="ID")
 curve( log(2*x), col="blue", add=TRUE)
 curve( log(x+1), col="yellow", add=TRUE)
 curve( log(x+0.5), col="green", add=TRUE)
 legend( "topleft", legend=c("sqrt(x)", "log(2*x)", "log(x+1)", "log(x+0.5)"), col=c("red", "blue", "yellow", "green"), pt.bg=c("red","blue", "yellow", "green"), pch=c(21,22))
 
-curve( sqrt(x), from=0, to=100, col="red", xlab="x", ylab="y")
+curve( sqrt(x), from=0, to=100, col="red", xlab="A/W", ylab="ID")
 curve( log(2*x), col="blue", add=TRUE)
 curve( log(x+1), col="yellow", add=TRUE)
 curve( log(x+0.5), col="green", add=TRUE)
