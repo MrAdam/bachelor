13f1330229f48d9c371e1ac2ea23f7ac8c80a627
Rasmus W. Andersen <williras@gmail.com>
2015-06-02 16:16:30 +0200

    hej

--- latex/arrows.r
+++ latex/arrows.r
@@ -7,7 +7,7 @@ library(gridExtra)
 
 # Load data from database
 options(warn=-1);
-connection <-dbConnect(dbDriver("MySQL"), user="root", password="toor", host="127.0.0.1", dbname="test")
+connection <-dbConnect(dbDriver("MySQL"), user="root", password="toor", host="127.0.0.1", dbname="finale")
 result <- dbSendQuery(connection, "SELECT * FROM person")
 persons <- dbFetch(result, n=-1)
 dbClearResult(result)
@@ -20,6 +20,18 @@ dbClearResult(result)
 dbDisconnect(connection)
 options(warn=0)
 
+mean(persons$age)
+length(persons[persons$gender=='male','gender'])
+length(persons[persons$gender=='female','gender'])
+length(persons[persons$videogames=='15 - 40 hours','videogames'])
+length(persons[persons$computers=='15 - 40 hours','computers'])
+persons
+persons[persons$id==35,]
+
+
+length(persons[persons$device=='mouse','device'])
+length(persons[persons$device=='touchpad','device'])
+length(persons[persons$age >40,])
 # Filter the data
 tasks <- subset(tasks, type == "pointing")
 limit <- sd(tasks$time) * 3
@@ -84,7 +96,7 @@ for (m_task in tasks[tasks$distance == 301 & tasks$width==16,]$id) {
 
 # OMREGNING AF PIXELS/MILISEC TIL CM/SEC -> X/3.7795
 # Plot the data
-plot1 <- ggplot(subset(points, task == 24), aes(x = elapsedTime, y = deltaDistance)) + geom_point() + coord_fixed(ratio=4)
+plot1 <- ggplot(subset(points, task == 21), aes(x = elapsedTime, y = deltaDistance)) + geom_point() + coord_fixed(ratio=4)
 print(plot1)
 
 plotNew <- ggplot(newPoints, aes(x = elapsedTime, y = deltaDistance)) + geom_point() + coord_fixed(ratio=4)
